61b9d6c8d7ee23f170c98a8e3bfe6c11b7eecb72
Sachin Mishra <152723124+sachin9935@users.noreply.github.com>
2024-04-06 22:16:21 +0530

    Change Type of variables

---
+++ Basic of Python/ChangeType.py
@@ -0,0 +1,12 @@
+new1=20
+new2=20.5
+add=new1+new2
+changetype=int(add)
+changetype1=complex(add)
+print(add)
+print(type(add))
+#change the type of variable..
+print(changetype)
+print(type(changetype))
+print(changetype1)
+print(type(changetype1))
