fa1f02a142211da0365f5db4f8faece20141dd5a
Sachin Mishra <152723124+sachin9935@users.noreply.github.com>
2024-04-07 23:29:00 +0530

    Methods

---
+++ Basic of Python/MethodInStr.py
@@ -0,0 +1,15 @@
+var1=" python programming "
+print(var1)
+print(var1.strip()) #Remove the whitespace beginning to end
+print(len(var1)) #count the number of  in strings
+print(var1.lower()) #change the string into lower case
+print(var1.upper()) #change the string into upper case
+print(var1.title()) #change the first char of each string
+print(var1.count("p")) #noumber of times occurs 
+print(var1.find("o")) #return 5
+print(var1.find("o",6)) #return 10
+print(var1.replace("y","m")) #return pmtyon 
+print(var1.split(",")) #change string into sun strings
+
+
+
