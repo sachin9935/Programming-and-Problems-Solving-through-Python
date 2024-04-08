4f19e764316d74f42195eec352d9eb2754d3272a
Sachin Mishra <152723124+sachin9935@users.noreply.github.com>
2024-04-08 22:01:30 +0530

    Update MethodInStr.py

--- Basic of Python/MethodInStr.py
+++ Basic of Python/MethodInStr.py
@@ -3,7 +3,7 @@ print(var1)
 print(var1.strip()) #Remove the whitespace beginning to end
 print(len(var1)) #count the number of  in strings
 print(var1.lower()) #change the string into lower case
-print(var1.upper()) #change the string into upper case
+print(var1.upper()) #change the string into upper  case
 print(var1.title()) #change the first char of each string
 print(var1.count("p")) #noumber of times occurs 
 print(var1.find("o")) #return 5
