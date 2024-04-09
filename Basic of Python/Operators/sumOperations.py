bbe15dd64a135941548c8721a6f0bb451be40562
Sachin Mishra <152723124+sachin9935@users.noreply.github.com>
2024-04-09 23:04:03 +0530

    Basic operations

---
+++ Basic of Python/Operators/sumOperations.py
@@ -0,0 +1,14 @@
+x=5
+y=10
+print("the answer of sum:",x+y)
+print("the answer of Sunbtraction:",x-y)
+print("the answer of Multipaction:",x*y)
+print("the answer of Devisin:",x/y)
+print("the answer of Modulus:",x%y)
+print("the answer of Exponent:",x**y)
+print("the answer of Devision(int):",x//y)
+print("the answer of Devision(Float):",-x//y)
+
+
+
+
