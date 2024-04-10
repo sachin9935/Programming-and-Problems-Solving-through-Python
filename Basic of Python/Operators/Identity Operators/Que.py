87c69ff25d71c940f850672ed5fcc1d54b2a4859
Sachin Mishra <152723124+sachin9935@users.noreply.github.com>
2024-04-10 23:45:15 +0530

    Identity Operators

---
+++ Basic of Python/Operators/Identity Operators/Que.py
@@ -0,0 +1,10 @@
+x=7
+y=8
+z=7
+print(id(x)) #id print the Address of memory...
+# print(id(y))
+# print(id(z))
+
+#a=x is z    #true, if the address x=z.
+b=x is not y #return true, if both variable are not same....
+print(b)
---
+++ Basic of Python/Operators/Identity Operators/deff.py
@@ -0,0 +1,7 @@
+"""this operators are used to campare the objecrts, 
+not if they are eQual but they are actualy same object, 
+with the same memorey location"""
+
+## opreators 
+is #return true if the both variable are same object
+is not #return true, if both variable are not same....
